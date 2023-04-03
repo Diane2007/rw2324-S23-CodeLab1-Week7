6fb31266e01dfbd193adb420a4613c1a25ea1061
diane2007 <wangruochong0406@gmail.com>
2023-04-03 01:35:39 -0400

    Finished

--- Trifles_Prototype1/Assets/Scripts/GameManager.cs
+++ Trifles_Prototype1/Assets/Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
 
@@ -42,21 +43,68 @@ public class GameManager : MonoBehaviour
         //change the title and description text into that of the currentLocation's
         title.text = currentLocation.locationName;
         description.text = currentLocation.locationDescription;
+        
+        //if currentLocation's upLocation doesn't exist
+        //up button is not interactable
+        if (!currentLocation.upLocation)
+        {
+            up.interactable = false;
+        }
+        else
+        {
+            up.interactable = true;
+            currentLocation.upLocation.downLocation = currentLocation;
+        }
+
+        //if currentLocation's downLocation doesn't exist
+        if (!currentLocation.downLocation)
+        {
+            down.interactable = false;
+        }
+        else
+        {
+            down.interactable = true;
+            currentLocation.downLocation.upLocation = currentLocation;
+        }
+        
+        //if current's left doesn't exist
+        if (!currentLocation.leftLocation)
+        {
+            left.interactable = false;
+        }
+        else
+        {
+            left.interactable = true;
+            currentLocation.leftLocation.rightLocation = currentLocation;
+        }
+        
+        //if current's right doesn't exist
+        if (!currentLocation.rightLocation)
+        {
+            right.interactable = false;
+        }
+        else
+        {
+            right.interactable = true;
+            currentLocation.rightLocation.leftLocation = currentLocation;
+        }
+        
+        
     }
 
     //init player
     public GameObject player;
     Vector3 playerStartPos;
-    int moveDistance = 65;
+    Vector3 playerPos;
+    int moveDistance = 20;
     
     void Start()
     {
         //start with the initial current location
         UpdateLocation();
         
-        //define player start position
-        playerStartPos = GetComponent<Transform>().position;
-        Debug.Log(playerStartPos);
+        //define player position
+        playerPos = player.transform.position;
     }
 
     //press button to move in locations
@@ -66,20 +114,27 @@ public class GameManager : MonoBehaviour
         {
             case 0:     //up
                 currentLocation = currentLocation.upLocation;
+                playerPos.y += moveDistance;
                 break;
             case 1:     //down
                 currentLocation = currentLocation.downLocation;
+                playerPos.y -= moveDistance;
                 break;
             case 2:     //left
                 currentLocation = currentLocation.leftLocation;
+                playerPos.x -= moveDistance;
                 break;
             case 3:     //right
                 currentLocation = currentLocation.rightLocation;
+                playerPos.x += moveDistance;
                 break;
         }
+
+        //update the player position
+        player.transform.position = playerPos;
         
         //if missing this, text won't update
         UpdateLocation();
     }
-    
+
 }
